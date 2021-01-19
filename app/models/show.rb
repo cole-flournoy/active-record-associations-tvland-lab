6f987b20f4e3b31c67f99b786f71ced1dcb40656
Cole Flournoy <flournoy.cole@gmail.com>
2021-01-19 17:51:50 -0500

    commit tree 1d4dc7e7d61fb9708844a2e49e5848d5a7f51eb8

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    belongs_to :network
+
+    def actors_list
+        chrs = self.characters
+        chrs.collect{ |chr| "#{chr.actor.first_name} #{chr.actor.last_name}"}
+    end
 end
